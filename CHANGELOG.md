c46021aa68ee3f770bc374f246406fdc4a53784f
javierfh03 <pachi030919@gmai.com>
2024-02-14 22:02:52 +0100

    Fix error message

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Fixed
+- @javierfh03 - Fix error message.
+
 ## [0.1.0] - 2024-02-04
 ### Added
 - @javierfh03 - Add command manager with units test
 
-[0.1.0]: https://github.com/Lagatrix/shell_executor_lib.git
+[Unreleased]: https://github.com/Lagatrix/shell_executor_lib
+[0.1.0]: https://github.com/Lagatrix/shell_executor_lib/releases/tag/0.1.0
--- src/shell_executor_lib/errors/command_error.py
+++ src/shell_executor_lib/errors/command_error.py
@@ -1,15 +1,16 @@
 """Represents an error response of shell command."""
 
-from dataclasses import dataclass
 
-
-@dataclass
 class CommandError(Exception):
-    """Represents an error response of shell command.
+    """Represents an error when attempting to manage a nonexistent group."""
 
-    Attributes:
-        status_code: The exit status of the command
-        response: Error message.
-    """
-    status_code: int
-    response: str
+    def __init__(self, status_code: int, response: str) -> None:
+        """Initialize the CommandError exception.
+
+        Attributes:
+            status_code: The exit status of the command
+            response: Error message.
+        """
+        self.status_code = status_code
+        self.response = response
+        super().__init__(self.response)
